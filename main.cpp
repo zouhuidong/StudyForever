5aaf5192c1a4d3d3e27ba83a93505eb8a0c28f77
huidong <61970628+zouhuidong@users.noreply.github.com>
2024-01-31 06:54:57 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include "Wnd.h"
 #include "Schedule.h"
+#include "ini.hpp"
 
 int main()
 {
 	Wnd wnd;
 	wnd.Create();
 
+	// 读取配置
+	int x = GetIniFileInfoInt(L"./settings.ini", L"Position", L"x", 100);
+	int y = GetIniFileInfoInt(L"./settings.ini", L"Position", L"y", 100);
+
 	Schedule schedule;
-	schedule.SetOrigin(1180, 20);	// 设置课程表位置（主屏幕坐标）
+	schedule.SetOrigin(x, y);	// 设置课程表位置（主屏幕坐标）
 	
 	while (true)
 	{
